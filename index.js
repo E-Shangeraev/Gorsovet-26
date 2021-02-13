fe7e2b7a3467c5003992fbd130d653a0224d29e1
E-Shangeraev <e.shangeraev@gmail.com>
2021-02-13 14:06:53 +0700

    '1'

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use('/contacts', contactsRoutes);
 app.use('/corruption', corruptionRoutes);
 app.use('/reception', receptionRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
